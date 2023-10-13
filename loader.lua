39edc0ed2e7280fa60f9440b2fe0b22f06bd8a2e
sdhhf <95890805+sdhhf1245@users.noreply.github.com>
2023-10-12 18:32:48 -0700

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -5,6 +5,7 @@ local placeid = game.PlaceId
 local games = {
     ["8204899140"] = "https://raw.githubusercontent.com/sdhhf1245/kirb/main/ff2.lua",
     ["8206123457"] = "https://raw.githubusercontent.com/sdhhf1245/kirb/main/ff2practice.lua",
+    ["8206126987"] = "https://raw.githubusercontent.com/sdhhf1245/kirb/main/ff2.lua",
     ["11736628397"] = "https://raw.githubusercontent.com/sdhhf1245/kirb/main/hoopslife.lua",
     ["7540727946"] = "https://raw.githubusercontent.com/sdhhf1245/kirb/main/hoopslife.lua",
     ["13883948660"] = "https://raw.githubusercontent.com/sdhhf1245/kirb/main/hoopslife.lua",
